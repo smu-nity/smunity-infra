a159cab0c4427e36e4d2b8275c1848d6ed0277e5
hyunmin0317 <hyunmin-choi@naver.com>
2025-01-06 22:19:13 +0900

    chore: pinpoint-agent 버전 수정 (#18)

--- pinpoint/agent/install.sh
+++ pinpoint/agent/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://repo1.maven.org/maven2/com/navercorp/pinpoint/pinpoint-agent/3.0.1/pinpoint-agent-3.0.1.tar.gz \
+wget https://repo1.maven.org/maven2/com/navercorp/pinpoint/pinpoint-agent/2.5.4/pinpoint-agent-2.5.4.tar.gz \
 && tar xvfz pinpoint-agent-*.tar.gz \
 && rm pinpoint-agent-*.tar.gz \
-&& mv pinpoint-agent-3.0.1 pinpoint-agent
+&& mv pinpoint-agent-2.5.4 pinpoint-agent
