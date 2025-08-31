2e8869d3db5367c5a76c3bf9b5e4ee89f7f67a20
hyunmin0317 <hyunmin-choi@naver.com>
2025-08-31 12:06:33 +0900

    refactor: 코드 리팩토링 (#40)

--- alertmanager/entrypoint.sh
+++ alertmanager/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# 환경변수를 sed로 치환해서 config.yml 생성
+# 환경변수를 sed로 치환해서 alertmanager.yml 생성
 sed "s|\${SLACK_WEBHOOK}|${SLACK_WEBHOOK}|g" \
-    /etc/alertmanager/config.yml.template \
-    > /etc/alertmanager/config.yml
+    /etc/alertmanager/alertmanager.yml.template \
+    > /etc/alertmanager/alertmanager.yml
 
 # Alertmanager 실행
-exec /bin/alertmanager --config.file=/etc/alertmanager/config.yml
+exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml
